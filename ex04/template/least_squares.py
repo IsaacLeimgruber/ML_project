f9579f5786a0b67468aa36eaee1b3d728fad3a9f
IsaacLeimgruber <isaac.leimgruber@epfl.ch>
2017-10-12 15:27:03 +0200

    Add bias-variance

--- ex04/template/least_squares.py
+++ ex04/template/least_squares.py
@@ -6,12 +6,14 @@ Least Square
 
 import numpy as np
 
+def compute_loss(y, tx, w):
+    e = y - tx @ w
+    loss = np.sum(e*e)/(2*len(e))
+    return loss;
 
 def least_squares(y, tx):
-    """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    """calculate the least squares solution."""
+    
+    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    loss = compute_loss(y, tx, w)
+    return w, loss;

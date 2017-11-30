4057fd208f6f60cdad5c510e4c61f21b694f0306
IsaacLeimgruber <isaac.leimgruber@epfl.ch>
2017-11-30 14:21:25 +0100

    test merge

--- project2/project_text_classification/run.py
+++ project2/project_text_classification/run.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 DATA_FOLDER = "Data/"
 # https://www.overleaf.com/12522751fsgrdpydrwxf READ and EDIT link for report
+# tes merge
 
 
 def main():
     print("hello")
-    #data_path = DATA_FOLDER + "data.csv"
+    # data_path = DATA_FOLDER + "data.csv"
     data_path = "sample_submission.csv"
     data = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
     print(data)

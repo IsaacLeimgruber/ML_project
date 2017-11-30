e03dab79fe66a2d16569b56990f9cfd211962442
IsaacLeimgruber <isaac.leimgruber@epfl.ch>
2017-11-30 13:34:20 +0100

    Create run.py, test load sample_submission

---
+++ project2/project_text_classification/run.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+DATA_FOLDER = "Data/"
+
+
+def main():
+    print("hello")
+    #data_path = DATA_FOLDER + "data.csv"
+    data_path = "sample_submission.csv"
+    data = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
